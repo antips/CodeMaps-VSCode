a901d3b7457a7b1513ffdba7e1edc7fc132f8a39
DEWERDT Antoine <antoinecodew@gmail.com>
2023-10-29 14:57:08 +0100

    Improve HelloWorld structure: activateCommands

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,35 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as helloWorld from './helloWorld';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "codemaps-vscode" is now active!');
+	activateCommands(context);
+}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('codemaps-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CodeMaps!');
-	});
+/**
+ * Activates the commands for the extension.
+ * 
+ * @param {vscode.ExtensionContext} context - The extension context.
+ */
+function activateCommands(context: vscode.ExtensionContext) {
+	/**
+	 * Structure to associate each command ID to a function.
+	 * @param {string} commandId - The ID of the command.
+	 * @param {Function} functionToCall - The function to call when the command is executed.
+	*/
+	let commandsToLink = {
+		/* eslint-disable @typescript-eslint/naming-convention */
+		'codemaps-vscode.helloWorld': helloWorld.helloWorld,
+		/* eslint-enable @typescript-eslint/naming-convention */
+	};
 
-	context.subscriptions.push(disposable);
+	/**
+	 * Registers the commands and adds them to the extension's subscriptions.
+	 */
+	for (const [commandId, functionToCall] of Object.entries(commandsToLink)) {
+		console.log(`commandId: ${commandId}, functionToCall: ${functionToCall}`);
+		context.subscriptions.push(vscode.commands.registerCommand(commandId, functionToCall));
+	}
 }
 
 // This method is called when your extension is deactivated
---
+++ src/helloWorld.ts
@@ -0,0 +1,5 @@
+import * as vscode from 'vscode';
+
+export function helloWorld() {
+    vscode.window.showInformationMessage('Hello World from CodeMaps!');
+}
